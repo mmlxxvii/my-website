6db13d77861e5e5454ed77e19f356b4681f8dfb1
mmlxxvii <96957399+MMLXXVII@users.noreply.github.com>
2023-02-03 22:24:49 -0300

    - try/catch add

--- src/scripts/get-github.ts
+++ src/scripts/get-github.ts
@@ -1,29 +1,35 @@
 import { GitHubAPI } from "@/@types/github-api"
 
-
 const getGithub = async (): Promise<Array<GitHubAPI> | null> => {
-    const url = "https://api.github.com/users/MMLXXVII/repos?sort=updated&direction=desc"
-    const res = await fetch(url)
-    const data = await res.json()
-    const repos: Array<GitHubAPI> = []
+    try {
+        const url = "https://api.github.com/users/MMLXXVII/repos?sort=updated&direction=desc"
+        const res = await fetch(url)
+        const data = await res.json()
+        const repos: Array<GitHubAPI> = []
 
-    if (!res) {
+        if (!res) {
+            return null
+        }
+
+        const l = data.length < 5 ? data.length : 5
+
+        for (let i = 0; i < l; i++) {
+            repos.push(
+                {
+                    name: data[i].name,
+                    url: data[i].html_url,
+                    updatedAt: data[i].updated_at
+                }
+            )
+        }
+
+        return repos
+
+    } catch (error) {
+        console.log(error)
         return null
+
     }
-
-    const l = data.length < 5 ? data.length : 5
-
-    for (let i = 0; i < l; i++) {
-        repos.push(
-            {
-                name: data[i].name,
-                url: data[i].html_url,
-                updatedAt: data[i].updated_at
-            }
-        )
-    }
-
-    return repos
 }
 
 export { getGithub }
